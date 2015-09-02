bc6443029ed23638685c3db060e9dfb71288f667
Ming-der Wang <ming@log4analytics.com>
2015-09-02 23:08:34 +0800

    new dockerize.sh

--- gen/genShellCode.go
+++ gen/genShellCode.go
@@ -16,21 +16,8 @@ var (
 	gingerTemplateShellCode = template.Must(template.New("ginger").Parse(
 		`#!/bin/bash
 set -o xtrace
-echo "start dockerizing ..."
-export FILE='./Makefile'
-if [ -f $FILE ]
-then
-       echo "File $FILE exist."
-else
-       echo "File $FILE does not exist."
-       go generate
-fi
-echo "start dockerizing ..."
-go build
-godep save -r
-docker build -t mingderwang/onion .
-docker run -i -t -v $(pwd):/gopath/src/onion mingderwang/onion onion migratedb
-docker run -d -p 8080:8080 -v $(pwd):/gopath/src/onion mingderwang/onion onion serve
+docker build -t ming/chilli .
+docker run -d -p 8080:80 ming/chilli
 `))
 )
 
